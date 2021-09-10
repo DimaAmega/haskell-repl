d5770baf8362b135c08f06b2cb263c6be04840fc
DimaAmega <dima.amega1@gmail.com>
2021-09-11 01:58:36 +0300

    code refactor

--- run_instructions.sh
+++ run_instructions.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-
 set -e
 
-if [ -f $1 ]; then
-    cat $1 | ghci $2 | sed -e '1,3d' -e 's/*Main> //g' -e '$ d'
+
+CURDIR=`/bin/pwd`
+BASEDIR=$(dirname $0)
+
+echo "CURDIR is $CURDIR"
+echo "BASEDIR is $BASEDIR"
+
+cd $1
+
+if [ -f $2 ]; then
+    cat $2 | ghci $3 -fno-show-loaded-modules | sed \
+    -e '/Ok, [A-Za-z0-9]* module\(s*\) loaded./,$!d' \
+    -e '/Ok, [A-Za-z0-9]* module\(s*\) loaded./d' \
+    -e 's/*[A-Z][A-Za-z0-9]*> *//' \
+    -e '$ d'
+    exit 0
 fi
+
+echo "no such file $2"
+exit 1
--- watch_haskell.sh
+++ watch_haskell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
 set -e
-nodemon --exec "./run_instructions.sh $1 $2" -e .hs,.txt
+
+nodemon --exec "./run_instructions.sh $1 $2 $3" -e .hs,.txt
